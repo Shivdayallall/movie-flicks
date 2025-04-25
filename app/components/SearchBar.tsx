1ff111de583ac8fc1c047832f0bdc2d6c976cff0
shivl <shivlall0009@gmail.com>
2025-04-25 17:30:29 -0400

    rendered the card component for the movies

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const SearchBar = ({placeholder, onPress}: Props) => {
   return (
-    <View className='flex-row items-center bg-light-200 rounded-full px-5 py-4'>
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
 
       <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
       <TextInput onPress={onPress} placeholder={placeholder} value='' onChangeText={() => {}} placeholderTextColor="#a8b5db" className='flex-1 ml-2 text-white' />
