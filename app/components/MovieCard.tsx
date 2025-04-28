dcfd2500a2c5e6ea419ac08d0657fcf7f555cf1a
shivl <shivlall0009@gmail.com>
2025-04-28 07:52:08 -0400

    completed home screen

--- app/components/MovieCard.tsx
+++ app/components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { icons } from '@/constants/icons'
 
 const MovieCard = ({id, title, poster_path, vote_average, release_date}:Movie) => {
   return (
@@ -16,7 +17,27 @@ const MovieCard = ({id, title, poster_path, vote_average, release_date}:Movie) =
                 
                 />
 
-                <Text className='text-sm font-bold text-white mt-2'>{title}</Text>
+                <Text className='text-sm font-bold text-white mt-2' numberOfLines={1}>{title}</Text>
+
+                <View className='flex-row items-center justify-start gap-x-1'>
+
+                    <Image source={icons.star} className='size-4'/>
+
+                    <Text className='text-xs text-white font-bold uppercase'>{Math.round(vote_average / 2)}</Text>
+
+                </View>
+
+                <View className='flex-row items-center justify-between'>
+
+                    <Text className='text-xs text-light-300 font-medium mt-1'>
+                        {release_date?.split('_')[0]}
+                    </Text>
+
+
+
+
+
+                </View>
                 
             </TouchableOpacity>
         
